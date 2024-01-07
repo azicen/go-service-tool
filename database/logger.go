6e282d4c1b6f7f3b172482afbfc3e0734602d81d
azicen <chenjiazi2000@163.com>
2024-01-07 15:18:59 +0000

    feat(database): 添加基于 kratos log 的 grom 日志接口实现

---
+++ database/logger.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"gorm.io/gorm/logger"
+)
+
+// GormLoggerHelper GORM Logger 接口的实现
+type GormLoggerHelper struct {
+	*log.Helper
+}
+
+// NewLoggerHelper 创建一个新的 GormLogger 实例
+func NewLoggerHelper(logger log.Logger) *GormLoggerHelper {
+	h := log.NewHelper(logger)
+	return &GormLoggerHelper{h}
+}
+
+// LogMode 设置日志级别
+func (g *GormLoggerHelper) LogMode(level logger.LogLevel) logger.Interface {
+	return g
+}
+
+// Info 日志信息级别
+func (g *GormLoggerHelper) Info(ctx context.Context, msg string, data ...interface{}) {
+	g.Infof(msg, data...)
+}
+
+// Warn 日志警告级别
+func (g *GormLoggerHelper) Warn(ctx context.Context, msg string, data ...interface{}) {
+	g.Warnf(msg, data...)
+}
+
+// Error 日志错误级别
+func (g *GormLoggerHelper) Error(ctx context.Context, msg string, data ...interface{}) {
+	g.Errorf(msg, data...)
+}
+
+// Trace 用于跟踪 SQL 执行和记录慢查询
+func (g *GormLoggerHelper) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	elapsed := time.Since(begin)
+	sql, rows := fc()
+
+	if err != nil {
+		g.Errorw("err", err, "elapsed", elapsed, "sql", sql, "rows", rows)
+	} else {
+		g.Debugw("elapsed", elapsed, "sql", sql, "rows", rows)
+	}
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-kratos/kratos/v2 v2.7.2
 	github.com/holiman/uint256 v1.2.4 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
