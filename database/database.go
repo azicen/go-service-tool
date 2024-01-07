f48d295d51f865ed77f8c45a34d9f436f8bf740e
azicen <chenjiazi2000@163.com>
2024-01-07 14:24:10 +0000

    feat(database): 添加grom钩子

---
+++ database/database.go
@@ -0,0 +1,71 @@
+package database
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID    uint64    `json:"id" gorm:"column:id; primaryKey"`
+	Ctime time.Time `json:"ctime" gorm:"column:ctime"` // 状态最后一次更改
+	Mtime time.Time `json:"mtime" gorm:"column:mtime"` // 数据最后一次修改
+}
+
+type DeleteModel struct {
+	Delete bool `json:"delete" gorm:"column:delete"` // 数据软删除
+}
+
+func RegisterCallbacks(db *gorm.DB) {
+	err := db.Callback().Create().Before("gorm:create").Replace("gorm:create_time_stamp", createCallback)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Update().Before("gorm:update").Replace("gorm:update_time_stamp", updateCallback)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Delete().Before("gorm:delete").Replace("gorm:delete_time_stamp", deleteCallback)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createCallback(db *gorm.DB) {
+	if db.Statement.Schema != nil {
+		nowTime := time.Now()
+		createTimeField := db.Statement.Schema.LookUpField("ctime")
+		if createTimeField != nil {
+			_ = createTimeField.Set(db.Statement.Context, db.Statement.ReflectValue, nowTime)
+		}
+		modifyTimeField := db.Statement.Schema.LookUpField("mtime")
+		if modifyTimeField != nil {
+			_ = modifyTimeField.Set(db.Statement.Context, db.Statement.ReflectValue, nowTime)
+		}
+	}
+}
+
+func updateCallback(db *gorm.DB) {
+	if db.Statement.Schema != nil {
+		nowTime := time.Now()
+		modifyTimeField := db.Statement.Schema.LookUpField("mtime")
+		if modifyTimeField != nil {
+			_ = modifyTimeField.Set(db.Statement.Context, db.Statement.ReflectValue, nowTime)
+		}
+	}
+}
+
+func deleteCallback(db *gorm.DB) {
+	if db.Statement.Schema != nil {
+		modifyDeleteField := db.Statement.Schema.LookUpField("delete")
+		if modifyDeleteField != nil {
+			_ = modifyDeleteField.Set(db.Statement.Context, db.Statement.ReflectValue, true)
+		}
+
+		nowTime := time.Now()
+		modifyTimeField := db.Statement.Schema.LookUpField("mtime")
+		if modifyTimeField != nil {
+			_ = modifyTimeField.Set(db.Statement.Context, db.Statement.ReflectValue, nowTime)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,15 @@ go 1.21.4
 require github.com/ethereum/go-ethereum v1.13.8
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
+
+require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/holiman/uint256 v1.2.4 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	gorm.io/gorm v1.25.5
 )
